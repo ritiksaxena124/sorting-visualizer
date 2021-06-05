db521cbe87bd6a5af2ded64926fac7bf66aa2481
Ritik Saxena <ritiksaxena124@gmail.com>
2021-06-05 23:22:03 +0530

    swap function shifted to sorting.js

--- js_files/bubble.js
+++ js_files/bubble.js
@@ -1,26 +1,4 @@
 // BUBBLE SORT
-
-// promise to swap two blocks
-
-function swap(el1, el2) {
-  return new Promise((resolve) => {
-    var temp1 = el1.style.height;
-    el1.style.height = el2.style.height;
-    el2.style.height = temp1;
-
-    var temp2 = el1.childNodes[0].innerHTML;
-    el1.childNodes[0].innerHTML = el2.childNodes[0].innerHTML;
-    el2.childNodes[0].innerHTML = temp2;
-
-    // for waiting of 0.25 sec
-    window.requestAnimationFrame(function () {
-      setTimeout(() => {
-        resolve();
-      }, 250);
-    });
-  });
-}
-
 async function BubbleSort(delay = 100) {
   var blocks = document.querySelectorAll(".block");
   for (var i = 0; i < blocks.length; i++) {
